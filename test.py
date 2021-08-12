e2c963ae63b9cd6413c674f76f71d4424b9989fc
ysh-1998 <yangsh824@gmail.com>
2021-08-12 10:36:36 +0800

    test

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def to_vector(model, batch_data, device,config):
     model.eval()
     with torch.no_grad():
         token_ids = batch_data["token_ids"].to(device) # [bs,30]or[bs,100]
-        print("token_ids:", token_ids.shape)
+        # print("token_ids:", token_ids.shape)
         attention_mask = batch_data["attention_mask"].to(
             device)
         vector = model(
@@ -23,7 +23,7 @@ def to_vector(model, batch_data, device,config):
             attention_mask=attention_mask
         ) # [bs,512]
         vector = F.normalize(vector, dim=1, p=2)
-        print("vector:",vector.shape,vector.device)
+        # print("vector:",vector.shape,vector.device)
 
     return vector
 
@@ -42,6 +42,8 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(config.PRE_TRAINED_MODEL_NAME)
     q_data_loader = get_query_data_loader(df_queries,tokenizer,config.max_len,config.q_batch_size)
     d_data_loader = get_doc_data_loader(df_docs,tokenizer,config.max_len,config.batch_size)
+    print("querys:{}个batch".format(len(q_data_loader)))
+    print("docs:{}个batch".format(len(d_data_loader)))
     
     model = get_model(config)
     model = model.to(device)
@@ -52,22 +54,30 @@ if __name__ == '__main__':
     d_id_list = []
     rank = []
     for i, q_batch in enumerate(q_data_loader):
+        # if i > 1:
+        #     break
+        print("querys:第{}/{}个batch".format(i,len(q_data_loader)))
         q_ids = q_batch["ids"].cpu().numpy().tolist()
         q_vector = to_vector(model,q_batch,device,config)
-        print("q_vector:", q_vector.shape,q_vector.device) # [qbs,512]
+        # print("q_vector:", q_vector.shape,q_vector.device) # [qbs,512]
         for j, d_batch in enumerate(d_data_loader):
+            # if j > 1:
+            #     break
+            print("querys:第{}/{}个batch".format(i,len(q_data_loader)))
+            print("docs:第{}/{}个batch".format(j,len(d_data_loader)))
             d_vector = to_vector(model,d_batch,device,config)
-            print("d_vector:", d_vector.shape,d_vector.device) # [dbs,512]
+            # print("d_vector:", d_vector.shape,d_vector.device) # [dbs,512]
             rel_score = torch.matmul(q_vector, d_vector.transpose(0,1)) # [qbs,dbs]
             if j == 0:
                 rel_scores = rel_score
             else:
                 rel_scores = torch.cat((rel_scores,rel_score),1) # [qbs,dbs*(j+1)]
-        for row in rel_scores.shape[0]:
-            unrank_doc = rel_scores[row].cpu().numpy()
-            df_docs[2] = unrank_doc
+        print("rel_scores:",rel_scores.shape,rel_scores.device)
+        for row in range(rel_scores.shape[0]):
+            unrank_doc_rel = rel_scores[row].cpu().numpy()
+            df_docs[2] = unrank_doc_rel
             df_docs = df_docs.sort_values(by=2,ascending=False)
-            rand_doc = df_docs[1].values.tolist()[:100]
+            rand_doc = df_docs[0].values.tolist()[:100]
             q_id_list += [q_ids[row]]*100
             d_id_list += rand_doc
             rank += range(1,101)
